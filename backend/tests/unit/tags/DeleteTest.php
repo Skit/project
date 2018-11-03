5569562f3fe917a895b79caf8ad1c0dbaf28578b
belianin <belianin_a@tanuki.ru>
2018-11-03 17:02:36 +0300

    Added tags model tests

---
+++ backend/tests/unit/tags/DeleteTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Start
+ * Date: 03.11.2018
+ * Time: 15:52
+ */
+
+namespace backend\tests\unit\tags;
+
+use Codeception\Test\Unit;
+
+class DeleteTest extends Unit
+{
+
+}
