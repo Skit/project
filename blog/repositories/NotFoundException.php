3440807c958274bb4d479fd39aaea56f685a6bb9
belianin <belianin_a@tanuki.ru>
2018-11-03 17:04:32 +0300

    Added tags model

---
+++ blog/repositories/NotFoundException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Start
+ * Date: 03.11.2018
+ * Time: 16:50
+ */
+
+namespace blog\repositories;
+
+
+class NotFoundException extends \DomainException
+{
+
+}
