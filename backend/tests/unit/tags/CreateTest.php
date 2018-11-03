b47cb0bd9cec9a48220c7d954af5a8ca3956c1f5
belianin <belianin_a@tanuki.ru>
2018-11-03 17:02:49 +0300

    Added tags model tests

---
+++ backend/tests/unit/tags/CreateTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Start
+ * Date: 03.11.2018
+ * Time: 15:55
+ */
+
+namespace backend\tests\unit\tags;
+
+use Codeception\Test\Unit;
+use blog\entities\Tags;
+
+class CreateTest extends Unit
+{
+
+    public function testSuccess(){
+
+        $tag = new Tags();
+    }
+
+    public function frequencyUpTest(){
+
+    }
+}
