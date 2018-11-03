0fe479000b80fcc89d518d1fa8c517539089827f
belianin <belianin_a@tanuki.ru>
2018-11-03 17:03:23 +0300

    Added tags model

---
+++ blog/repositories/TagsRepositories.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Start
+ * Date: 03.11.2018
+ * Time: 16:42
+ */
+
+namespace blog\repositories;
+
+use blog\entities\Tags;
+use blog\repositories\NotFoundException;
+
+class TagRepository
+{
+    public function get($id): Tags
+    {
+        if (!$tag = Tags::findOne($id)) {
+            throw new NotFoundException('Tag is not found.');
+        }
+        return $tag;
+    }
+    public function findByName($name): Tags
+    {
+        if (!$tag = Tags::findOne(['name' => $name])) {
+            throw new NotFoundException('Tag is not found.');
+        }
+        return $tag;
+    }
+    public function save(Tags $tag): void
+    {
+        if (!$tag->save()) {
+            throw new \RuntimeException('Saving error.');
+        }
+    }
+    public function remove(Tags $tag): void
+    {
+        if (!$tag->delete()) {
+            throw new \RuntimeException('Removing error.');
+        }
+    }
+}
