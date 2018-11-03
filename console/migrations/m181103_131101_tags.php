ecb975c47ee7e2011791f7d370d305b9a0da5bfa
belianin <belianin_a@tanuki.ru>
2018-11-03 17:01:48 +0300

    Unsigned added

--- console/migrations/m181103_131101_tags.php
+++ console/migrations/m181103_131101_tags.php
@@ -16,11 +16,10 @@ class m181103_131101_tags extends Migration
             'id' => $this->primaryKey()->unsigned(),
             'name' => $this->string(50)->notNull()->unique(),
             'slug' => $this->string(50)->notNull(),
-            'frequency' => $this->smallInteger()->notNull()->unsigned()->defaultValue(1),
+            'frequency' => $this->smallInteger()->unsigned()->defaultValue(1),
             'is_active' => $this->smallInteger(1)->unsigned()->defaultValue(1),
         ]);
     }
-
     /**
      * @inheritdoc
      */
@@ -28,19 +27,4 @@ class m181103_131101_tags extends Migration
     {
         $this->dropTable('{{%tags}}');
     }
-
-    /*
-    // Use up()/down() to run migration code without a transaction.
-    public function up()
-    {
-
-    }
-
-    public function down()
-    {
-        echo "m181103_131101_tags cannot be reverted.\n";
-
-        return false;
-    }
-    */
 }
