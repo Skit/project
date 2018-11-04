c8bd561a194ac75fb0bc5de6e95039603f448d81
belianin <belianin_a@tanuki.ru>
2018-11-04 10:43:43 +0300

    Encode fix

--- console/migrations/m181103_131101_tags.php
+++ console/migrations/m181103_131101_tags.php
@@ -12,13 +12,15 @@ class m181103_131101_tags extends Migration
      */
     public function safeUp()
     {
+        $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+
         $this->createTable('{{%tags}}', [
             'id' => $this->primaryKey()->unsigned(),
             'name' => $this->string(50)->notNull()->unique(),
             'slug' => $this->string(50)->notNull(),
             'frequency' => $this->smallInteger()->unsigned()->defaultValue(1),
             'is_active' => $this->smallInteger(1)->unsigned()->defaultValue(1),
-        ]);
+        ], $tableOptions);
     }
     /**
      * @inheritdoc
