c75d7e0c03e3422b2a7544ffe67493c6d823b1b4
belianin <belianin_a@tanuki.ru>
2018-11-07 08:52:50 +0300

    Yii2 test bug fix

--- backend/config/test.php
+++ backend/config/test.php
@@ -8,5 +8,12 @@ return [
         'urlManager' => [
             'showScriptName' => true,
         ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=yii2advanced_test',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ],
     ],
 ];
