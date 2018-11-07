6433913ed7050edfbf6a00c5eb413e32e6bf33a6
belianin <belianin_a@tanuki.ru>
2018-11-07 09:06:13 +0300

    Tags added

---
+++ backend/models/Tags.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Start
+ * Date: 03.11.2018
+ * Time: 19:17
+ */
+
+namespace backend\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+
+/**
+ * Class Tags
+ *
+ * @package blog\entities
+ * @property integer id
+ * @property string name
+ * @property string slug
+ * @property integer frequency
+ * @property integer is_active
+ */
+class Tags extends ActiveRecord
+{
+    const
+        DEFAULT_IS_ACTIVE = 1,
+        DEFAULT_FREQUENCY = 1;
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%tags}}';
+    }
+
+    public function beforeSave($insert)
+    {
+        $this->frequency = self::DEFAULT_FREQUENCY;
+        $this->is_active = self::DEFAULT_IS_ACTIVE;
+
+        return parent::beforeSave($insert);
+    }
+
+    /**
+     * @param string $name
+     * @param string $slug
+     * @return Tags
+     */
+    public static function create(string $name, string $slug): self
+    {
+        $tag = new self();
+        $tag->name = $name;
+        $tag->slug = $slug;
+
+        return $tag;
+    }
+
+    /**
+     * Set rows as [[name=>tagName, slug=>tagSlug]]
+     *
+     * @param array $rows
+     * @return int
+     * @throws \yii\db\Exception
+     */
+    public static function batchCreate(array $rows): int
+    {
+        return Yii::$app->db->createCommand()
+            ->batchInsert(self::tableName(), ['name', 'slug'], $rows)
+            ->execute();
+    }
+
+    /**
+     * @param int $value
+     * @return int
+     */
+    public function updateFrequency(int $value=1): int
+    {
+        return $this->updateCounters(['frequency' => $value]);
+    }
+}
