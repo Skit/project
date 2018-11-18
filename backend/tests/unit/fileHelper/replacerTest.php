5e8a6485bff863875b485b6f940cba8e3c22a02a
belianin <belianin_a@tanuki.ru>
2018-11-18 14:30:26 +0300

    Updated replacers tests

--- backend/tests/unit/fileHelper/replacerTest.php
+++ backend/tests/unit/fileHelper/replacerTest.php
@@ -22,21 +22,36 @@ class replacerTest extends Unit
     }
 
     public function testCorrectSetVariable(){
-        $string = FileHelper::replacer('{:date}');
-        expect($string)->equals(date('Y/m'));
+        $string = FileHelper::replacer('{salt}', ['{salt}' => 'qwerty']);
+        expect($string)->equals('qwerty');
+    }
+
+    public function testCorrectSetFunction(){
+        $string = FileHelper::replacer('{:date}', ['{:date}' => 'Y']);
+        expect($string)->equals(date('Y'));
+    }
+
+    public function testMixesSet(){
+        $string = FileHelper::replacer('path/{:date}/{salt}', ['{salt}' => 'qwerty', '{:date}'=>'m']);
+        expect($string)->equals('path/' . date('m') . '/qwerty');
+    }
+
+    public function testReplaceNotAssociativeArray(){
+        $string = FileHelper::replacer('path/{:date}/{date}/{salt}', ['m', '{date}'=>date('H'), 'qwerty']);
+        expect($string)->equals('path/' . date('m/') . date('H/') .'qwerty');
     }
 
     public function testUndefinedVariableException()
     {
         $this->expectException(\RuntimeException::class);
-        $this->expectExceptionMessage('Undefined variable: {foo}, {bar}');
+        $this->expectExceptionMessage('Undefined variable: {foo}');
         FileHelper::replacer('{:date}/{foo}/{bar}');
     }
 
     public function testUndefinedFunctionException()
     {
         $this->expectException(\RuntimeException::class);
-        $this->expectExceptionMessage('Undefined function: foo');
+        $this->expectExceptionMessage('Undefined function: {:foo}');
         FileHelper::replacer('{:foo}');
     }
 }
