bbf3536b33ed604eed2d779d2b9c8081cf6175f3
belianin <belianin_a@tanuki.ru>
2018-11-21 09:11:28 +0300

    replacerTest added

--- backend/tests/unit/fileHelper/replacerTest.php
+++ backend/tests/unit/fileHelper/replacerTest.php
@@ -23,7 +23,7 @@ class replacerTest extends Unit
 
     public function testCorrectSetVariable(){
         $string = FileHelper::replacer('{salt}', ['{salt}' => 'qwerty']);
-        expect($string)->equals('qwerty');
+        expect($string)->equals('d8578e');
     }
 
     public function testCorrectSetFunction(){
@@ -33,12 +33,12 @@ class replacerTest extends Unit
 
     public function testMixesSet(){
         $string = FileHelper::replacer('path/{:date}/{salt}', ['{salt}' => 'qwerty', '{:date}'=>'m']);
-        expect($string)->equals('path/' . date('m') . '/qwerty');
+        expect($string)->equals('path/' . date('m') . '/d8578e');
     }
 
     public function testReplaceNotAssociativeArray(){
         $string = FileHelper::replacer('path/{:date}/{date}/{salt}', ['m', '{date}'=>date('H'), 'qwerty']);
-        expect($string)->equals('path/' . date('m/') . date('H/') .'qwerty');
+        expect($string)->equals('path/' . date('m/') . date('H/') .'d8578e');
     }
 
     public function testUndefinedVariableException()
